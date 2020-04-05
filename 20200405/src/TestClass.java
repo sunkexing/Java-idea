ea3705a0103811eb26d4d58947b4a472e1481e20
sunkexing <1633896174@qq.com>
2020-04-05 19:57:54 +0800

    .

---
+++ 20200405/src/TestClass.java
@@ -0,0 +1,19 @@
+
+
+
+public class TestClass {
+
+
+
+    private static void testMethod(){
+
+        System.out.println("testMethod");
+
+    }
+
+    public static void main(String[] args) {
+
+        ((TestClass)null).testMethod();
+
+    }
+}
